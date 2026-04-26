7229e0726ded1e12a51369f41ba89c16ac4200e8
Yonatan Pizarro <yonatan.pizarro@safetyclub.cl>
2026-04-26 00:01:08 -0400

    avances

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,33 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
+  // CONFIGURACIÓN DE RUTAS PARA SUBRUTAS
+  app: {
+    baseURL: "/encuestas/",
+    buildAssetsDir: "assets", // Evita conflictos con la carpeta _nuxt por defecto
+  },
+
   devServer: {
     port: 3003,
     host: "0.0.0.0",
   },
+
   // Habilitamos la estructura de Nuxt 4
   future: {
     compatibilityVersion: 4,
   },
 
-  // Agregamos el módulo de BootstrapVueNext
   modules: ["@bootstrap-vue-next/nuxt"],
 
-  // Cargamos los estilos de Bootstrap
   css: [
     "bootstrap/dist/css/bootstrap.min.css",
-    "bootstrap/dist/css/bootstrap.min.css", // Tu bootstrap actual
-    "bootstrap-icons/font/bootstrap-icons.css", // Nueva línea
+    "bootstrap-icons/font/bootstrap-icons.css",
   ],
 
-  // Esto ayuda a resolver problemas de compilación con componentes específicos
   build: {
     transpile: ["bootstrap-vue-next"],
   },
+
   runtimeConfig: {
-    // Estas variables solo están disponibles en el servidor
     adminUser: process.env.ADMIN_USER,
     adminPass: process.env.ADMIN_PASS,
     jwtSecret: process.env.JWT_SECRET,
